c7336b9cd0bc5628374063b717940cc26c89f763
yu-san51 <nekonyan.yuki51@gmail.com>
2020-03-25 06:02:26 +0000

    [Finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a1e97952a31fecf6403b8e19c384d4defb08af581078f7b8b354ff369f93377919bf3f3f6a5f98f1662a430fe65ca810b228c65d4bc673ddb58c147b2f3ea365'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+	root "post_images#index"
+    devise_for :users
+    resources :post_images, only:[:new, :create, :index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
