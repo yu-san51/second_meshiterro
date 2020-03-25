c69d104de300967ea34fffe6a1364895cba0138b
yu-san51 <nekonyan.yuki51@gmail.com>
2020-03-25 08:58:06 +0000

    [Finish] 5-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
 	root "post_images#index"
     devise_for :users
-    resources :post_images, only:[:new, :create, :index, :show] do
+    resources :post_images, only:[:new, :create, :index, :show, :destroy] do
     	resource :post_comments, only: [:create, :destroy]
     	resource :favorites, only: [:create, :destroy]
     end
+    resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
